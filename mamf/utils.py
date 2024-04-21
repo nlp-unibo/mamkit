d1addc6a86d04e5d91572173e67bfa339fa22d1c
Samuele Marro <marrosamuele@gmail.com>
2024-04-21 11:55:00 +0200

    Added utils.

---
+++ mamf/utils.py
@@ -0,0 +1,26 @@
+import lightning as L
+
+
+class MAMFLightingModel(L.LightningModule):
+    def __init__(self, model, loss_function, optimizer_class, **optimizer_kwargs):
+        super().__init__()
+        self.model = model
+        self.loss_function = loss_function
+        self.optimizer_class = optimizer_class
+        self.optimizer_kwargs = optimizer_kwargs
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        inputs = { k: v for k, v in batch.items() if k != 'targets' }
+
+        y_hat = self.model(**inputs)
+        loss = self.loss_function(y_hat, batch['targets'])
+        return loss
+
+    def configure_optimizers(self):
+        return self.optimizer_class(self.model.parameters(), **self.optimizer_kwargs)
+
+def to_lighting_model(model, loss_function, optimizer_class, **optimizer_kwargs):
+    return MAMFLightingModel(model, loss_function, optimizer_class, **optimizer_kwargs)
