57008583455211ce8b024c16b5c81afe416e812b
AndreaZecca <andreazecca03@gmail.com>
2024-04-28 19:14:00 +0200

    Starting pre-computed dataset support

--- mamkit/datasets/core.py
+++ mamkit/datasets/core.py
@@ -4,42 +4,23 @@ import torch.utils
 from typing import Tuple
 
 class MAMKitDataset(torch.utils.data.Dataset):
-    # returns [text_features, text_attentions, audio_features, audio_attentions, target]
-    def __getitem__(self, idx) -> Tuple[list, list, list, list, list]:
+    def __init__(self, dataframe):
+        self.dataframe = dataframe
+    
+    def download_audio(self):
+        raise NotImplementedError
+        
+    def precompute_text(self):
         raise NotImplementedError
 
+    def precompute_audio(self):
+        raise NotImplementedError
 
-class MAMKitModularDataset(torch.utils.data.Dataset):
-    def __init__(self, dataset, text_preprocessor, audio_preprocessor, target_preprocessor):
-        super().__init__()
-        self.dataset = dataset
-        self.text_preprocessor = text_preprocessor
-        self.audio_preprocessor = audio_preprocessor
-        self.target_preprocessor = target_preprocessor
-
-
+class MAMKitPrecomputedDataset(torch.utils.data.Dataset):
+    def __init__(self, text_features, audio_features, labels):
+        self.text_features = text_features
+        self.audio_features = audio_features
+        self.labels = labels
+    
     def __getitem__(self, idx):
-        text, audio, target = self.dataset[idx]
-        
-        text_data = self.text_preprocessor(text) # epected to return a list of features or a list of features and attentions
-        audio_data = self.audio_preprocessor(audio) # epected to return a list of features or a list of features and attentions
-
-        target = self.target_preprocessor(target)
-
-        return text_data, audio_data, target
-
-    def __len__(self):
-        return len(self.text_dataset)
-
-
-
-class MAMKitListDataset(torch.utils.data.Dataset):
-    def __init__(self, *datasets):
-        assert all(len(datasets[0]) == len(dataset) for dataset in datasets)
-        self.datasets = datasets
-
-    def __len__(self):
-        return len(self.datasets[0])
-
-    def __getitem__(self, idx):
-        return tuple(dataset[idx] for dataset in self.datasets)
+        return self.text_features[idx], self.audio_features[idx], self.labels[idx]
--- mamkit/utils.py
+++ mamkit/utils.py
@@ -1,26 +1,119 @@
-import lightning as L
+# import lightning as L
+import os
+from mamkit.datasets import MAMKitPrecomputedDataset
+import pandas as pd
+import torch
+
+SUPPORTED_DATASETS = {
+    'usdbelec' : {
+        'audio' : {
+            'wavlm-single': 'https://huggingface.co/datasets/andreazecca3/wavlm-single/resolve/main/WavLMsingle.zip',
+            'wavlm-downsampled': ...,
+        },
+        'text' : {
+            'bert' : ...
+        }
+    }
+}
+
+# class MAMKitLightingModel(L.LightningModule):
+#     def __init__(self, model, loss_function, optimizer_class, **optimizer_kwargs):
+#         super().__init__()
+#         self.model = model
+#         self.loss_function = loss_function
+#         self.optimizer_class = optimizer_class
+#         self.optimizer_kwargs = optimizer_kwargs
+
+#     def forward(self, x):
+#         return self.model(x)
+
+#     def training_step(self, batch, batch_idx):
+#         inputs = { k: v for k, v in batch.items() if k != 'targets' }
+
+#         y_hat = self.model(**inputs)
+#         loss = self.loss_function(y_hat, batch['targets'])
+#         return loss
+
+#     def configure_optimizers(self):
+#         return self.optimizer_class(self.model.parameters(), **self.optimizer_kwargs)
+
+# def to_lighting_model(model, loss_function, optimizer_class, **optimizer_kwargs):
+#     return MAMKitLightingModel(model, loss_function, optimizer_class, **optimizer_kwargs)
 
 
-class MAMKitLightingModel(L.LightningModule):
-    def __init__(self, model, loss_function, optimizer_class, **optimizer_kwargs):
-        super().__init__()
-        self.model = model
-        self.loss_function = loss_function
-        self.optimizer_class = optimizer_class
-        self.optimizer_kwargs = optimizer_kwargs
 
-    def forward(self, x):
-        return self.model(x)
+def load_text(dataset_name):
+    if dataset_name.lower() not in SUPPORTED_DATASETS.keys():
+        raise ValueError(f'Dataset {dataset_name} not supported. Supported datasets: {SUPPORTED_DATASETS.keys()}')
+    if dataset_name.lower() == 'usdbelec':
+        path = './data/MM-USElecDeb60to16.csv'
+        text_column = 'Text'
+        set_column = 'Set'
+        splits = ['TRAIN', 'VALIDATION', 'TEST']
+    df = pd.read_csv(path)
+    train = df[df[set_column] == splits[0]][text_column].tolist()
+    val = df[df[set_column] == splits[1]][text_column].tolist()
+    test = df[df[set_column] == splits[2]][text_column].tolist()
+    return train, val, test
 
-    def training_step(self, batch, batch_idx):
-        inputs = { k: v for k, v in batch.items() if k != 'targets' }
+def load_labels(dataset_name):
+    if dataset_name.lower() not in SUPPORTED_DATASETS.keys():
+        raise ValueError(f'Dataset {dataset_name} not supported. Supported datasets: {SUPPORTED_DATASETS.keys()}')
+    if dataset_name.lower() == 'usdbelec':
+        path = './data/MM-USElecDeb60to16.csv'
+        label_column = 'Component'
+        set_column = 'Set'
+        splits = ['TRAIN', 'VALIDATION', 'TEST']
+    df = pd.read_csv(path)
+    train = df[df[set_column] == splits[0]][label_column].tolist()
+    val = df[df[set_column] == splits[1]][label_column].tolist()
+    test = df[df[set_column] == splits[2]][label_column].tolist()
+    return train, val, test
 
-        y_hat = self.model(**inputs)
-        loss = self.loss_function(y_hat, batch['targets'])
-        return loss
 
-    def configure_optimizers(self):
-        return self.optimizer_class(self.model.parameters(), **self.optimizer_kwargs)
+def get_dataset(dataset_name, text_preprocessing=None, audio_preprocessing=None, download_dir='./data'):
+    if dataset_name.lower() not in SUPPORTED_DATASETS.keys():
+        raise ValueError(f'Dataset {dataset_name} not supported. Supported datasets: {SUPPORTED_DATASETS.keys()}')
+    
+    ### Text preprocessing
+    if not callable(text_preprocessing):
+        raise ValueError(f'Text preprocessing must be a callable. Received: {text_preprocessing}')
+    else:
+        raw_text_train, raw_text_val, raw_text_test = load_text(dataset_name)
+        text_train = list(map(text_preprocessing, raw_text_train))
+        text_val = list(map(text_preprocessing, raw_text_val))
+        text_test = list(map(text_preprocessing, raw_text_test))
 
-def to_lighting_model(model, loss_function, optimizer_class, **optimizer_kwargs):
-    return MAMKitLightingModel(model, loss_function, optimizer_class, **optimizer_kwargs)
+    ### Audio preprocessing
+    if isinstance(audio_preprocessing, str):
+        if audio_preprocessing.lower() not in SUPPORTED_DATASETS[dataset_name]['audio'].keys():
+            raise ValueError(f'Audio preprocessing {audio_preprocessing} not supported. Supported audio preprocessing: {SUPPORTED_DATASETS[dataset_name]["audio"].keys()}')
+        download_link = SUPPORTED_DATASETS[dataset_name]['audio'][audio_preprocessing.lower()]
+        dataset_dir = os.path.join(download_dir, dataset_name, audio_preprocessing)
+        if not os.path.exists(dataset_dir):
+            os.makedirs(dataset_dir)
+        dataset_path = os.path.join(dataset_dir, 'dataset.zip')
+        if not os.path.exists(dataset_path):
+            print(f'Downloading dataset {audio_preprocessing}...')
+            os.system(f'wget {download_link} -O {dataset_path} >/dev/null 2>&1')
+            print(f'Dataset {audio_preprocessing} downloaded.')
+            print(f'Extracting dataset {audio_preprocessing}...')
+            os.system(f'unzip {dataset_path} -d {dataset_dir} >/dev/null 2>&1')
+            print(f'Dataset {audio_preprocessing} extracted.')
+        audio_train, audio_val, audio_test = torch.load(os.path.join(dataset_dir, 'train.pkl')), torch.load(os.path.join(dataset_dir, 'val.pkl')), torch.load(os.path.join(dataset_dir, 'test.pkl'))
+        os.system(f'rm -rf {os.path.join(download_dir, dataset_name)} >/dev/null 2>&1')
+    elif callable(audio_preprocessing) or audio_preprocessing is None:
+        # Load the raw version of the dataset
+        # Optionally apply the audio preprocessing
+        ...
+    else:
+        raise ValueError(f'Audio preprocessing must be a string or a callable. Received: {audio_preprocessing}')
+
+    ### Labels
+    labels_train, labels_val, labels_test = load_labels(dataset_name)
+
+    train = MAMKitPrecomputedDataset(text_train, audio_train, labels_train)
+    val = MAMKitPrecomputedDataset(text_val, audio_val, labels_val)
+    test = MAMKitPrecomputedDataset(text_test, audio_test, labels_test)
+
+    return train, val, test
